87d90acde8b74c32588eb483533e65be5451842e
Arokiyaraja <aland.arokiyaraja@hotmail.com>
2023-02-09 16:20:19 +0100

    layout generated

--- onlinemarket.py
+++ onlinemarket.py
@@ -11,7 +11,12 @@ def index():
 
 @app.route('/product')
 def product():
-    return render_template('product.html')
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+    return render_template('product.html', products=items)
 
 
 if __name__ == '__main__':
