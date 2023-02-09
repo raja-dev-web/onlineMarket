00386f5d43495bd1184565fd940d92636a1e5b81
Arokiyaraja <aland.arokiyaraja@hotmail.com>
2023-02-09 15:44:20 +0100

    Product page added

--- onlinemarket.py
+++ onlinemarket.py
@@ -9,5 +9,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/product')
+def product():
+    return render_template('product.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
